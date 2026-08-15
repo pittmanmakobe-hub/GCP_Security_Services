05f88bd603fc705ccbcb5d6e83f33618fd847cf4
Makobe Pittman <pittmanmakobe@gmail.com>
2026-08-15 13:22:20 -0500

    Add Terraform configuration for AWS provider

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
